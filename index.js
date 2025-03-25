6895e06f4f2568269649c5af0272e88c630acf43
4Bee <109135643+4BFC@users.noreply.github.com>
2025-03-25 20:44:17 +0900

    🔀: CLI 배포하기 (#2)
    * ✨: npm 으로 배포하기 위한 준비
    
    * ✨: 고유한 이름 작성
    
    * ✨: 고유한 이름 작성
    
    - 네이밍 규칙상 대문자는 불가능
    
    ---------
    
    Co-authored-by: 4BFC <4bee.code@gamil.com>

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("당신의 이름은 무엇인가요? ", (answer) => {
-  console.log(`안녕하세요, ${answer}님! 👋`);
+rl.question("What is your name? ", (answer) => {
+  console.log(`Hello, ${answer}! 👋`);
   rl.close();
 });
